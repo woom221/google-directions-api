199550bc804fd1f3384b52277a7cd915fe95c308
woom221 <sean.yi@mail.utoronto.ca>
2024-04-08 22:26:09 -0400

    added docstrings

--- src/main.py
+++ src/main.py
@@ -10,15 +10,27 @@ import re
 
 CLEANR = re.compile('<.*?>')
 
+# for cleaning purpose
 def cleanhtml(raw_html):
     cleantext = re.sub(CLEANR, '', raw_html)
     return cleantext
 
+# import CSV, request google distances API and export the resulting data
 class GoogleDistanceCalc:
     def set_up(self):
+        """
+        Set up the google map API
+        :return: googlemaps object with assigned client
+        """
         self.client = googlemaps.Client(key='REPLACE WITH API KEY')
 
     def read_from_csv(self, file):
+        """
+        Parse the CSV file and collect addresses of departure and arrival
+
+        :param file: file to read from
+        :return: dictionary of address of departure and arrival
+        """
         temp_dict = {}
         data = pd.read_csv(file)
         origins = data['Origin Address'].tolist()
@@ -36,6 +48,14 @@ class GoogleDistanceCalc:
         return temp_dict
 
     def retrieve_matrix(self, data, file_name):
+        """
+        Request Google Distances API to collected relevant information such as
+        traveling duration and routes then export the information to CSV file
+
+        :param data: dictionary of addresses (generated from read_from_csv)
+        :param file_name: file name to export
+        :return: None
+        """
         requested_time_IST = []
         durations = []
         durations_in_traffic = []
@@ -43,12 +63,18 @@ class GoogleDistanceCalc:
         routes = []
         start_address = []
         end_address = []
+
+        # collect information from the origin address
         for i in range(len(data['origins'])):
             ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%m-%d %H:%M:%S.%f')
             requested_time_IST.append(ind_time)
+
+            # request travelling information from origins to destinations
             directions = self.client.directions(origin=data["origins"][i], destination=data["destinations"][i], mode='driving', departure_time=datetime.now())
             directions = directions[0]
             inner_lst = []
+
+            # parsing the html outcome to readable data
             for j in range(len(directions['legs'][0]['steps'])):
                 inner_lst.append(cleanhtml(directions['legs'][0]['steps'][j]["html_instructions"]))
             routes.append(inner_lst)
@@ -57,6 +83,8 @@ class GoogleDistanceCalc:
             distance.append(str(round(directions['legs'][0]['distance']['value'] / 1000, 3)) + " km")
             start_address.append(directions['legs'][0]['start_address'])
             end_address.append(directions['legs'][0]['end_address'])
+
+        # export to CSV file
         df_builder = {'requested time': requested_time_IST, 'origin': start_address, 'destination': end_address,
                       'duration': durations, 'duration_in_traffic': durations_in_traffic, 'distance': distance, 'routes': routes}
         df = pd.DataFrame(data=df_builder)
